18cfeb942c9723ea4aa92bc5768b4e6bf7a243f4
ahmsay <ahmsay@gmail.com>
2021-01-03 13:22:46 +0300

    event is added

---
+++ sender/src/main/java/com/rabbitmq/sender/event/SomethingHappenedEvent.java
@@ -0,0 +1,16 @@
+package com.rabbitmq.sender.event;
+
+import java.io.Serializable;
+
+public class SomethingHappenedEvent implements Serializable {
+
+    private final String message;
+
+    public SomethingHappenedEvent(final String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
