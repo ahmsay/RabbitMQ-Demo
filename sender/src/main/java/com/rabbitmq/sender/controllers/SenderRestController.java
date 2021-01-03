8d0eaa697bfe75f36644777999e8d69d25a1d769
ahmsay <ahmsay@gmail.com>
2021-01-03 13:42:18 +0300

    sending the event

--- sender/src/main/java/com/rabbitmq/sender/controllers/SenderRestController.java
+++ sender/src/main/java/com/rabbitmq/sender/controllers/SenderRestController.java
@@ -1,15 +1,30 @@
 package com.rabbitmq.sender.controllers;
 
+import com.rabbitmq.sender.event.EventDispatcher;
+import com.rabbitmq.sender.event.SomethingHappenedEvent;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
 public class SenderRestController {
+
+    private final EventDispatcher eventDispatcher;
+
+    public SenderRestController(final EventDispatcher eventDispatcher) {
+        this.eventDispatcher = eventDispatcher;
+    }
 
     @GetMapping()
     public String connect() {
         return "Success";
+    }
+
+    @PostMapping("/send")
+    public String sendEvent(@RequestParam final String message) {
+        System.out.println("Sending the event...");
+        eventDispatcher.send(new SomethingHappenedEvent(message));
+        return "Event is sent.";
     }
 }
