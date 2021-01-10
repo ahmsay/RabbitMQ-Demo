0710ff2958d9a4d2b039c670ff0f12f5028cedad
ahmsay <ahmsay@gmail.com>
2021-01-10 12:38:44 +0300

    event will be send as request body

--- sender/src/main/java/com/rabbitmq/sender/controllers/SenderRestController.java
+++ sender/src/main/java/com/rabbitmq/sender/controllers/SenderRestController.java
@@ -4,7 +4,7 @@ import com.rabbitmq.sender.event.EventDispatcher;
 import com.rabbitmq.sender.event.SomethingHappenedEvent;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,9 +22,9 @@ public class SenderRestController {
     }
 
     @PostMapping("/send")
-    public String sendEvent(@RequestParam final String message) {
+    public String sendEvent(@RequestBody final SomethingHappenedEvent event) {
         System.out.println("Sending the event...");
-        eventDispatcher.send(new SomethingHappenedEvent(message));
+        eventDispatcher.send(event);
         return "Event is sent.";
     }
 }
