6223da79875e0bf4b083f5e5fd83d176634f5048
ahmsay <ahmsay@gmail.com>
2021-01-10 11:47:17 +0300

    DO NOT FORGET BEAN ANNOTATIONS

--- receiver/src/main/java/com/rabbitmq/receiver/configuration/RabbitMQConfiguration.java
+++ receiver/src/main/java/com/rabbitmq/receiver/configuration/RabbitMQConfiguration.java
@@ -8,9 +8,11 @@ import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
 import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
 
+@Configuration
 public class RabbitMQConfiguration implements RabbitListenerConfigurer {
 
     @Bean
